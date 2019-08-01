60498b2766e6293115741b5a99eee91cc9e707a9
Victor Boissiere <victor.boissiere@neo9.fr>
2019-08-01 18:27:03 +0200

    metrics: Add basic metric

--- pkg/api/server.go
+++ pkg/api/server.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -18,12 +19,22 @@ type HttpServer struct {
 	Port int32
 }
 
+func metricsRouter() http.Handler {
+	promHandler := func(next http.Handler) http.Handler { return promhttp.Handler() }
+	emptyHandler := func(w http.ResponseWriter, r *http.Request) {}
+	r := chi.NewRouter()
+	r.Use(promHandler)
+	r.Get("/", emptyHandler)
+	return r
+}
+
+
 func (server *HttpServer) Start() {
 
 	r := chi.NewRouter()
 	r.Use(middleware.Recoverer)
 
-	// r.Mount("/metrics", metricsRouter())
+	r.Mount("/metrics", metricsRouter())
 
 	log.Error(http.ListenAndServe(fmt.Sprintf(":%v", server.Port), r))
 }
