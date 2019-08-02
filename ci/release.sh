e6718ef0616b7cfff52a7bd0e078e12fd0b97837
Victor Boissiere <victor.boissiere@neo9.fr>
2019-08-02 11:55:46 +0200

    ci: Fix docker tag

--- ci/release.sh
+++ ci/release.sh
@@ -13,7 +13,7 @@ fi
 if ! [ -z "$TRAVIS_TAG" ]; then
     echo "Release $TRAVIS_TAG"
     echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_LOGIN" --password-stdin
-    docker tag neo9sas/mongodb-backups:latest neo9sas/mongodb-backup:$TRAVIS_TAG
+    docker tag neo9sas/mongodb-backups:latest neo9sas/mongodb-backups:$TRAVIS_TAG
     docker push neo9sas/mongodb-backups:$TRAVIS_TAG
 fi
 
